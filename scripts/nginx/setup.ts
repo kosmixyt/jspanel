1c0574fd634b3f754c41bdd9371b7da24c8ee8f5
= <=>
2025-09-19 15:02:47 +0000

    feat: Enhance SiteOptions interface with additional properties for SSL, PHP options, and logging

--- scripts/nginx/setup.ts
+++ scripts/nginx/setup.ts
@@ -23,11 +23,44 @@ SetupNginx()
 
 export interface SiteOptions {
     domains: Domain[];
-    Ssl: null | { id: string; path: string; keyPath: string; };
-    php: null | { version: string; socketPath: string; };
+    // ssl must include all domains in the domains array
+    Ssl: null | { id: string; path: string; keyPath: string; redirectHttp: boolean };
+    php: null | {
+        version: string;
+        socketPath: string;
+        phpOptions: { [key: string]: string | number | boolean };
+    };
+    // 404 : /custom_404.html
+    // 500 : /custom_500.html
+    ErrorPagesMapping: { [key: number]: string };
+    indexes: string[];
     rootPath: string;
+    // Paths: ({ path: string } & (RestrictedPath | ProxyOptions & Header & IpRestriction))[];
+    Paths: {
+        path: string;
+        elements: (RestrictedPath | (ProxyOptions & { Headers?: Header[] } & IpRestriction))[];
+    }[]
+    AccessLogPath: string;
+    ErrorLogPath: string;
+
 }
 
+export interface RestrictedPath {
+    path: string;
+    password: string;
+}
+export interface IpRestriction {
+    allow: string[];
+    deny: string[];
+}
+export interface Header {
+    name: string;
+    value: string;
+}
+export interface ProxyOptions {
+    target: string;
+    Websocket: boolean;
+}
 class NginxManager {
     static async restart() {
         console.log("Restarting Nginx...")
